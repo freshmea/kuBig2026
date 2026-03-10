ec51f1ba768dc69883dd9c1989c3c81ad5d2df57
freshmea <freshmea@gmail.com>
2026-03-10 14:27:00 +0900

    fhar

--- c_example/fahr2Celcius.c
+++ c_example/fahr2Celcius.c
@@ -1,1 +1,15 @@
 // Fahr 온도값을 넣어서 Celsius 온도값을 반환 하도록 코드를 작성하세요.
+#include <stdio.h>
+
+int main()
+{
+    int fahr;
+    printf("F 온도 를 넣으세요: \n");
+    scanf("%d", &fahr);
+    double celsius;
+    celsius = 5.0 / 9.0 * ((double)fahr - 32.0);
+
+    printf("F(fahr) 온도 : %d\n", fahr);
+    printf("C(celsius) 온도 : %.2f\n", celsius);
+    return 0;
+}
