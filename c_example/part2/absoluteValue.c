af1ad5887215b9763d0175361d52e85fb2b5c11b
freshmea <freshmea@gmail.com>
2026-03-11 09:50:21 +0900

    passFail

--- c_example/part2/absoluteValue.c
+++ c_example/part2/absoluteValue.c
@@ -6,10 +6,10 @@ int main(void)
     printf("숫자를 넣으세요 :");
     scanf("%d", &num);
 
-    if (num < 0){
-        num = -num;
-    }
+    // if (num < 0){
+    //     num = -num;
+    // }
 
-    printf("절대값은 : %d\n", num);
+    printf("절대값은 : %d\n", num < 0 ? -num : num );
     return 0;
 }
