9e2fd10bde3dc7dc3ddebd59d3cac64f69c4eb8d
freshmea <freshmea@gmail.com>
2026-03-12 16:42:56 +0900

    arraycompare

--- c_example/part3/pointer2.c
+++ c_example/part3/pointer2.c
@@ -2,18 +2,14 @@
 
 int main(void)
 {
-    int var_a;
-    int *pVar_a;
-    int **ppVar_a;
+    // p가 주소를 가리켜야 된다.
+    //  int *p;
+    //  *p = 200;
 
-    var_a = 100;
-    pVar_a = &var_a;
-    ppVar_a = &pVar_a;
-
-    **ppVar_a = 200;
-    printf("var_a: %d, &var_a: %p\n", var_a, &var_a);
-    printf("pVar_a: %p, *pVar_a: %d\n", pVar_a, *pVar_a);
-    printf("ppVar_a: %p, *ppVar_a: %p **ppVar_a: %d\n", ppVar_a, *ppVar_a, **ppVar_a);
+    // 주소를 가리키면 오류가 나지 않음.
+    int a;
+    int *p;
+    p = &a;
 
     return 0;
 }
