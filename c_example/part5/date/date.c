57aa9032751130f11806d1d4d4ca86b7a0dc19e5
freshmea <freshmea@gmail.com>
2026-03-16 10:58:52 +0900

    date

---
+++ c_example/part5/date/date.c
@@ -0,0 +1,12 @@
+#include "date.h"
+
+void printDate(const Date *pDate)
+{
+    printf("%d-%d-%d\n", pDate->year, pDate->month, pDate->day);
+}
+void setDate(Date *pDate, const int year, const int month, const int day)
+{
+    pDate->year = year;
+    pDate->month = month;
+    pDate->day = day;
+}
