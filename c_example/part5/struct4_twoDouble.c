af15e7efeb804b65c5cac1e7457290d44cbd3782
freshmea <freshmea@gmail.com>
2026-03-16 11:51:07 +0900

    struct4

---
+++ c_example/part5/struct4_twoDouble.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct
+{
+    double left;
+    double right;
+}Vision;
+
+void exchange(Vision *robot);
+
+int main(void)
+{
+    Vision robot;
+
+    printf("시력 입력: ");
+    scanf("%lf %lf", &robot.left, &robot.right);
+    exchange(&robot);
+    printf("바뀐 시력 : %.1lf %.1lf\n", robot.left, robot.right);
+    return 0;
+}
+
+void exchange(Vision *robot)
+{
+    double temp;
+    temp = robot->left;
+    robot->left = robot->right;
+    robot->right = temp;
+}
