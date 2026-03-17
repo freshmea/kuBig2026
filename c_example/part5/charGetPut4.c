25ac3974660419c7b1b7121090f418f5f13e8e46
freshmea <freshmea@gmail.com>
2026-03-17 10:34:10 +0900

    charGetPut4

--- c_example/part5/charGetPut4.c
+++ c_example/part5/charGetPut4.c
@@ -13,5 +13,12 @@ int main(void)
     putc(ch, stdout);
     fflush(stdout);
     sleep(1);
+    char *string1 = "스트링1 배열입니다.";
+    puts(string1); // puts 은 작동 방식이 마지막에 개행을 자동 추가한다.
+    sleep(1);
+    printf("프로그램 끝입니다.");
+    fputs(string1, stdout); // 개행 추가가 없다.
+    sleep(1);
+    printf("프로그램 끝입니다.");
     return 0;
 }
