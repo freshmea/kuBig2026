70e01ca8940770b297f0a2e2703b07a191e5e151
freshmea <freshmea@gmail.com>
2026-03-17 16:28:57 +0900

    printResult

--- c_example/part6/scoreProcess/main.c
+++ c_example/part6/scoreProcess/main.c
@@ -69,3 +69,33 @@ int main(void){
     fclose(fout);
     return 0;
 }
+
+void inputData(FILE *fp, Sdata *s, int n)
+{
+    for (int i = 0; i < n;++i){
+        fscanf(fp, "%s %d %d %d", s[i].name, &s[i].kor, &s[i].eng, &s[i].mat);
+    }
+}
+void calculateScore(Sdata *s, int n)
+{
+    printf("함수");
+}
+void calculateRank(Sdata *s, int n)
+{
+    printf("함수");
+}
+void sortPointers(Sdata *s, Sdata **table, int n)
+{
+    for (int i = 0; i < n;++i)
+        table[i] = &s[i];
+}
+void printResult(FILE *fp, Sdata **table, int n)
+{
+    fprintf(fp, "-----------------------------------------------\n");
+    fprintf(fp, "                  Test Result\n");
+    fprintf(fp, "-----------------------------------------------\n");
+    for (int i = 0; i < n;++i){
+        fprintf(fp, "%-20s %3d %3d %3d %3d %6.2f %2d\n", table[i]->name, table[i]->kor, table[i]->eng, table[i]->mat, table[i]->sum, table[i]->average, table[i]->rank);
+    }
+    fprintf(fp, "-----------------------------------------------\n");
+}
