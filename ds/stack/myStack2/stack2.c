fbe643169dbef07d583a2d4ef7b6a678f54ca628
freshmea <freshmea@gmail.com>
2026-03-18 11:31:34 +0900

    전위 후위 활용

--- ds/stack/myStack2/stack2.c
+++ ds/stack/myStack2/stack2.c
@@ -7,12 +7,14 @@ void initStack(Stack *ps)
 }
 void push(Stack *ps, int data)
 {
-    *ps->tos = data;
-    ++ps->tos;
+    // *ps->tos = data;
+    // ++ps->tos;
+    *(ps->tos++) = data;
 }
 
 int pop(Stack *ps)
 {
-    --ps->tos;
-    return *ps->tos;
+    // --ps->tos;
+    // return *ps->tos;
+    return *(--ps->tos);
 }
