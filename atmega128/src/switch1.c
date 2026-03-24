54eb304dabddf0909a006679fe5754c8b67be01e
freshmea <freshmea@gmail.com>
2026-03-24 10:47:42 +0900

    switch2

--- atmega128/src/switch1.c
+++ atmega128/src/switch1.c
@@ -13,7 +13,12 @@ int main(void)
 {
     DDRC = 0x0F; // led 설정
     DDRE = 0x00; // switch 설정
-    while(1){
-        PORTC = PINE >> 4;
+    uint8_t switch0;
+    while (1)
+    {
+        switch0 = PINE; // 0b 0101 0000
+        PORTC = switch0 >> 4; // 0b 0000 0101
+        // ... 시간이 걸리는 코드
+        //... 인터럽트로 스위치 제어
     }
 }
