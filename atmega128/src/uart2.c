5af9f5e14b33ce9d455aa6cd8c6ee9b713f53764
freshmea <freshmea@gmail.com>
2026-03-25 16:53:44 +0900

    uart3

--- atmega128/src/uart2.c
+++ atmega128/src/uart2.c
@@ -3,8 +3,6 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-
-
 FILE OUTPUT = FDEV_SETUP_STREAM(uart0Transmit, NULL, _FDEV_SETUP_WRITE);
 FILE INPUT = FDEV_SETUP_STREAM(NULL, uart0Receive, _FDEV_SETUP_READ);
 
