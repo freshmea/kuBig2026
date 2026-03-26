77ad3c62116da1ae67a96e23fb903945df39b60c
freshmea <freshmea@gmail.com>
2026-03-26 14:37:17 +0900

    pwmled

---
+++ atmega128/src/pwmled.c
@@ -0,0 +1,27 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRC = 0x0F;
+    DDRB |= _BV(PB4);
+
+    TCCR0 = _BV(WGM00) | _BV(WGM01) | _BV(COM01) | _BV(CS01);
+    // fast PWM, com -> clear, 8 분주비
+
+    uint8_t brightness = 0;
+    int8_t delta = 1;
+
+    while(1)
+    {
+        OCR0 = brightness;
+        _delay_ms(10);
+        brightness += delta;
+        if (brightness == 0 || brightness == 255)
+        {
+            delta = -delta;
+        }
+    }
+    return 0;
+}
