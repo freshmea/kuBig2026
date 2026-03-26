ea71bd7f025af3ff039f4fe35f25fae06e5a1fb1
freshmea <freshmea@gmail.com>
2026-03-26 10:35:00 +0900

    github

---
+++ atmega128/src/cds2.c
@@ -0,0 +1,44 @@
+#include "lcd.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <string.h>
+#include <util/delay.h>
+
+volatile uint16_t ADC_result = 0;
+
+int main(void)
+{
+    DDRB = 0x10;
+    TCCR0 = 0x62;
+    TCNT0 = 0x00;
+    ADMUX = 0x41;   // ADC0 선택, single mode, 1번 채널, 3.3V 외부 기준 전압 (AREF)
+    ADCSRA = 0xAF;  // 10101111 ADC 허가, free running Mode, Interrupt 허가, 128 분주
+    ADCSRA |= 0x40; // ADSC AD 개시(Start)
+    sei();
+
+    lcdInit();
+    lcdGotoXY(0, 0);
+    lcdPrintData("Variable regist", 15);
+
+    while (1)
+    {
+        OCR0 = ADC_result;
+
+        {
+            char buf[16];
+            sprintf(buf, "R:%u   ", ADC_result);
+            lcdGotoXY(0, 1);
+            lcdPrintData(buf, strlen(buf));
+        }
+        _delay_ms(200);
+    }
+    return 0;
+}
+
+ISR(ADC_vect)
+{
+    cli();
+    ADC_result = ADC;
+    sei();
+}
--- doc/atmega128.md
+++ doc/atmega128.md
@@ -58,12 +58,28 @@ sudo udevadm control --reload-rules
 sudo udevadm trigger
 ```
 
-- 1교시:
-- 2교시:
-- 3교시:
+- 1교시: 8 bit 타이머 카운트/ 16 비트 타이머 카운트
+- 2교시: 타이머 인터럽트 예제 설명, timer1com.c 작성
+- 3교시: output compare 인터럽트 예제 설명, timer2ledleftright.c 작성
 - 4교시: USART 개념 설명, 레지스트리 설명
 - 5교시: uart1  uart1.c 작성
 - 6교시: uart2 예제 설명, stdio.h 사용하기, 외부 라이브러리 사용하기
 - 7교시: lcd 사용방법 - lcd.c, uart3 예제 설명,
 - 8교시: uart3 예제 실습 - 컴퓨터에서  atmega로 보낼때 3개 이상 character 보내면 깨지는 문제가 있음
+- 과제 : 없음
+
+---
+
+## 2026-3-26
+
+---
+
+- 1교시: ADC 개념 설명, 레지스트리 설명, adc.c 작성
+- 2교시: 가변 저항 읽기
+- 3교시:
+- 4교시:
+- 5교시:
+- 6교시:
+- 7교시:
+- 8교시:
 - 과제 : 없음
