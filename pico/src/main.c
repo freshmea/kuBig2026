cafbfd6eb23933d89b439c7cd38fdc996802f5e5
freshmea <freshmea@gmail.com>
2026-03-30 10:36:02 +0900

    pico -blink

--- pico/src/main.c
+++ pico/src/main.c
@@ -1,6 +1,7 @@
 // sudo apt update
 // sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential libstdc++-arm-none-eabi-newlib
-
+// git -C /home/aa/kuBig2026/pico/deps/pico-sdk submodule update --init
+// git submodule update --init
 int main()
 {
     return 0;
