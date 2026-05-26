5afa9384bf9928bcf05409ed90d4a01aa5b2ca17
freshmea <freshmea@naver.com>
2026-05-26 12:24:59 +0900

    _ 의 쓰임

--- python_example/basic/a70.poperty.py
+++ python_example/basic/a70.poperty.py
@@ -5,6 +5,7 @@ class Circle:
     def __init__(self, radius):
         self.__radius = radius
 
+    # Descriptor -> attr , getattr, setattr, delattr
     @property
     def radius(self):
         return self.__radius
@@ -33,6 +34,20 @@ def main():
     circle.radius = 3.14
     circle.radius = -5
     print(circle.get_area())
+
+    # descriptor
+    print(circle.__dict__)
+    print(vars(circle))
+    print(getattr(circle, "get_area"))
+    print(getattr(circle, "get_area")())
+    print(getattr(circle, "get_area2", None))
+
+    # _ 의 쓰임
+    for _ in range(100):
+        print("loop", end="")
+    a_tu = 1, 2, 3, "valuable text"
+    _, _, _, text = a_tu
+    print(text)
 
 
 if __name__ == "__main__":
