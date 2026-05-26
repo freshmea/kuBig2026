82d25e836273caae19066b8bce4288292423f5d6
freshmea <freshmea@naver.com>
2026-05-26 11:34:33 +0900

    class method

--- python_example/basic/a67_class_variable.py
+++ python_example/basic/a67_class_variable.py
@@ -1,5 +1,6 @@
 class Student:
     count = int()
+    students = list()
 
     def __init__(self, name, korean, math, english, science):
         self.name = name
@@ -8,6 +9,7 @@ class Student:
         self.english = english
         self.science = science
         Student.count += 1
+        Student.students.append(self)
         self.__aa = "secret key"
 
     def get_sum(self):
@@ -20,12 +22,11 @@ class Student:
         return f"{self.name}\t {self.korean}\t {self.math}\t {self.english}\t {self.science}"
 
     def __repr__(self):
-        return f"{self.name}\t {self.korean}\t {self.math}\t {self.english}\t {self.science}\
+        return f"{self.name}\t {self.korean}\t {self.math}\t {self.english}\t {self.science}\t\
             {self.get_sum()}\t {self.get_average()}"
 
     def __str__(self) -> str:
-        return f"{self.name}\t {self.korean}\t {self.math}\t {self.english}\t {self.science}\
-            {self.get_sum()}\t {self.get_average()} str"
+        return f"{self.name}\t {self.korean}\t {self.math}\t {self.english}\t {self.science}\t {self.get_sum()}\t {self.get_average()}"
 
     # 사칙연산 + - * /
     def __add__(self, other):
@@ -49,24 +50,26 @@ class Student:
             # raise
             return "error"
 
+    @classmethod
+    def print(cls):
+        print(f"등록된 학생 수는 {Student.count}")
+        print("이름\t 국어\t 수학\t 영어\t 과학\t 총점\t 평균")
+        print()
+        print("--------------------학생 목록------------------")
+        for student in Student.students:
+            print(student)
+        print("----------------------------------------------")
+
 
 def main():
-    students = [
-        Student("abc", 34, 65, 35, 94),
-        Student("gdf", 34, 45, 45, 50),
-        Student("wtr", 36, 75, 63, 94),
-        Student("nbd", 47, 65, 85, 70),
-        Student("ujd", 88, 95, 75, 33),
-        Student("efg", 64, 65, 55, 40),
-        Student("dgd", 33, 25, 75, 93),
-    ]
-    print(f"등록된 학생 수는 {Student.count}")
-    print("student[0] > student[1]", students[0] > students[1])
-    print("student[0] > student[1]", students[0] > 90)
-    print("student[0] + student[1]", students[0] + students[1])
-    print("student[0] - student[1]", students[0] - students[1])
-    print("student[0] * student[1]", students[0] * students[1])
-    print("student[0] / student[1]", students[0] / students[1])
+    Student("abc", 34, 65, 35, 94)
+    Student("gdf", 34, 45, 45, 50)
+    Student("wtr", 36, 75, 63, 94)
+    Student("nbd", 47, 65, 85, 70)
+    Student("ujd", 88, 95, 75, 33)
+    Student("efg", 64, 65, 55, 40)
+    Student("dgd", 33, 25, 75, 93)
+    Student.print()
 
 
 if __name__ == "__main__":
