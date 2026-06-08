ae4b4e0cd17d1c6bfdda6c368511837f7054189d
freshmea <freshmea@gmail.com>
2026-06-08 10:50:21 +0900

    사진 출력

--- opencv_ex/part1/01_helloWorld.cpp
+++ opencv_ex/part1/01_helloWorld.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
+
+String folderPath = "/home/aa/kuBig2026/opencv_ex/data/";
 
 int main()
 {
     cout << "hello, world" << endl;
+    Mat img = imread(folderPath + "20260604_113829.jpg");
+    // Mat img = Mat::zeros(480, 640, CV_8UC3);
+    Size size(640, 480);
+    resize(img, img, size);
+    imshow("lenna", img);
+    waitKey();
+
     return 0;
 }
