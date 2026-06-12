9ae79b383ff176ef39892852f5cac0ce71454cf5
freshmea <freshmea@gmail.com>
2026-06-12 10:03:08 +0900

    qr

--- opencv_ex/part5/50_qr.cpp
+++ opencv_ex/part5/50_qr.cpp
@@ -17,6 +17,11 @@ void decode_qrcode()
 {
     VideoCapture cap(0);
 
+    cap.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(CAP_PROP_FPS, 30);
+
     if (!cap.isOpened())
     {
         cerr << "Camera open failed!" << endl;
